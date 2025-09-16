851a7d8cadb66912709a3c3f0d0b76b55aab9254
IslandHopper-403 <59448185+IslandHopper-403@users.noreply.github.com>
2025-09-16 21:32:18 +0700

    Update navigation.js removed screen type from social to null

--- javascript/ui/navigation.js
+++ javascript/ui/navigation.js
@@ -87,12 +87,11 @@ export class NavigationManager {
     
    showScreen(screenType, updateHistory = true) {
        console.log(`📱 Navigating to ${screenType} screen`);
-        
-        // Check guest mode restrictions
-        // if (this.state.get('isGuestMode') && screenType === 'social') {
-         //  alert('🔒 Sign up to access social features and connect with other travelers!');
-         //  if (this.authManager) {
-              //  this.authManager.showRegister();
+         Check guest mode restrictions
+         if (this.state.get('isGuestMode') && screenType === 'null') {
+          alert('🔒 Sign up to access social features and connect with other travelers!');
+          if (this.authManager) {
+               this.authManager.showRegister();
            }
           return;
        }

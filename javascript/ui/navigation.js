604bda3993113400444139c423d437d43d352bb8
IslandHopper-403 <59448185+IslandHopper-403@users.noreply.github.com>
2025-09-16 21:25:08 +0700

    Update navigation.js Reverted back

--- javascript/ui/navigation.js
+++ javascript/ui/navigation.js
@@ -89,12 +89,12 @@ export class NavigationManager {
        console.log(`📱 Navigating to ${screenType} screen`);
         
         // Check guest mode restrictions
-        // if (this.state.get('isGuestMode') && screenType === 'social') {
-          //  alert('🔒 Sign up to access social features and connect with other travelers!');
-          //  if (this.authManager) {
-         //       this.authManager.showRegister();
-        //    }
-        //    return;
+         if (this.state.get('isGuestMode') && screenType === 'social') {
+           alert('🔒 Sign up to access social features and connect with other travelers!');
+           if (this.authManager) {
+               this.authManager.showRegister();
+          }
+          return;
        }
         
         // Update state

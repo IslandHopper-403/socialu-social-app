40dcd3c46665fdf174eb9b0af97815650a36e435
IslandHopper-403 <59448185+IslandHopper-403@users.noreply.github.com>
2025-10-01 14:59:29 +0700

    FavoritesCarousel.js -𝐙 Z-index Fix 🔧

--- javascript/features/favoritesCarousel.js
+++ javascript/features/favoritesCarousel.js
@@ -174,7 +174,7 @@ extractBusinessIdFromCard(cardElement) {
                     position: fixed;
                     left: 0;
                     top: 200px;
-                    z-index: 205;
+                    z-index: 275;
                     display: flex;
                     align-items: center;
                     transition: transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);

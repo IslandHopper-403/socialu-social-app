681c6a8b018301003d84a8fc286f870ac058f14e
IslandHopper-403 <59448185+IslandHopper-403@users.noreply.github.com>
2025-10-05 05:54:56 +0700

    Profile.js - index.html - Index.html - Cull Zodiac selections 003 - Horoscope Calc Added

--- javascript/features/profile.js
+++ javascript/features/profile.js
@@ -141,6 +141,53 @@ export class ProfileManager {
         this.navigationManager.closeOverlay('profileEditor');
         this.isEditing = false;
     }
+
+    /**
+     * Calculate zodiac sign from birthday
+     */
+    calculateZodiac(birthday) {
+        if (!birthday) return null;
+        
+        const date = new Date(birthday);
+        const day = date.getDate();
+        const month = date.getMonth() + 1; // 0-indexed
+        
+        const zodiacSigns = [
+            { sign: '♑ Capricorn', start: [12, 22], end: [1, 19] },
+            { sign: '♒ Aquarius', start: [1, 20], end: [2, 18] },
+            { sign: '♓ Pisces', start: [2, 19], end: [3, 20] },
+            { sign: '♈ Aries', start: [3, 21], end: [4, 19] },
+            { sign: '♉ Taurus', start: [4, 20], end: [5, 20] },
+            { sign: '♊ Gemini', start: [5, 21], end: [6, 20] },
+            { sign: '♋ Cancer', start: [6, 21], end: [7, 22] },
+            { sign: '♌ Leo', start: [7, 23], end: [8, 22] },
+            { sign: '♍ Virgo', start: [8, 23], end: [9, 22] },
+            { sign: '♎ Libra', start: [9, 23], end: [10, 22] },
+            { sign: '♏ Scorpio', start: [10, 23], end: [11, 21] },
+            { sign: '♐ Sagittarius', start: [11, 22], end: [12, 21] }
+        ];
+        
+        for (const { sign, start, end } of zodiacSigns) {
+            const [startMonth, startDay] = start;
+            const [endMonth, endDay] = end;
+            
+            if (startMonth === endMonth) {
+                if (month === startMonth && day >= startDay && day <= endDay) return sign;
+            } else {
+                if ((month === startMonth && day >= startDay) || (month === endMonth && day <= endDay)) {
+                    return sign;
+                }
+            }
+        }
+        
+        return null;
+    }
+
+    /**
+     * Load profile data into form
+     */
+    loadProfileDataIntoForm() {
+        const profile = this.state.get('userProfile');
     
     /**
      * Load profile data into form
